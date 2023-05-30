7757b08e1ddb1027394e0caec9776ee24be84274
ZahraaRashid <108655392+ZahraaRashid@users.noreply.github.com>
2023-05-30 16:38:44 +0300

    Add files via upload

---
+++ Activity11_Accordion/accordion.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$("#accordion").accordion({
+	  collapsible: true,
+	  heightStyle: "content",
+	  icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" }
+	});
+  });
+
