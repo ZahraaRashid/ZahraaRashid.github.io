7a7f672f7826f32adbd9e1a67e48446951acfc0d
ZahraaRashid <108655392+ZahraaRashid@users.noreply.github.com>
2023-05-30 16:36:09 +0300

    Delete Activity11_Accordion directory

--- Activity11_Accordion/accordion.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$("#accordion").accordion({
-	  collapsible: true,
-	  heightStyle: "content",
-	  icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" }
-	});
-  });
-
