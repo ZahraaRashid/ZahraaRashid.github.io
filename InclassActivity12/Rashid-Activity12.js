a56a46d43c3beec295256139a08d92ca3887b5b7
ZahraaRashid <108655392+ZahraaRashid@users.noreply.github.com>
2023-06-04 20:45:27 +0300

    Add files via upload

---
+++ InclassActivity12/Rashid-Activity12.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  function fetchSpeakerData(filename) {
+    $.getJSON("json_files/" + filename + ".json", function(data) {
+      $("main").empty();
+      var image = $("<img>").attr(data.speakers[0].image);
+      var title = $("<h1>").text(data.speakers[0].title);
+      var description = $("<p>").text(data.speakers[0].text);
+      var month = $("<h2>").html(data.speakers[0].month + "<br>" + data.speakers[0].speaker);
+      $("main").append(title, image, month, description);
+    });
+  }
+  $("#nav_list a").click(function(event) {
+    event.preventDefault();
+    var title = $(this).attr("title");
+    fetchSpeakerData(title);
+  });
+  });
